1bd4dd98a963fba2c9d3d660672765c8c3f825f4
bernard Puel <bernard.puel@st.com>
2026-02-16 15:08:31 +0100

    trusted-firmware-m_doc updates for v6.2.0

--- trusted-firmware-m_doc/_static/documentation_options.js
+++ trusted-firmware-m_doc/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v2.1.0-stm32mp-r1',
+    VERSION: 'v2.1.3-stm32mp-r2',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
